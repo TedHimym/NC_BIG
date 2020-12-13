59bfc97bcea1b3f140dc5011dec757f4a32502f4
zspc <w8866kl@163.com>
2020-12-13 08:07:49 +0800

    1214_zs_

--- contourLplot.m
+++ contourLplot.m
@@ -4,9 +4,8 @@ while (LI ~= layer)
     ind = ind + X(2, ind) + 1;
     LI = LI + 1;
 end
-plot(X(2, ind+1:ind+X(2,ind)), X(1, ind+1:ind+X(2,ind)))
-axis equal; axis([0 4 0 1])
-pause(0.03)
-axis equal; axis([0 4 0 1])
+% plot(X(2, ind+1:ind+X(2,ind)), X(1, ind+1:ind+X(2,ind)))
+% axis equal; axis([0 4 0 1])
+% pause(0.03)
 L = max(X(2, ind+1:ind+X(2,ind))) - R;
 end 
--- inject_an.m
+++ inject_an.m
@@ -2,6 +2,8 @@ clc; clear;
 
 load('inject.mat', 't_c');
 load('inject.mat', 'iosT');
+load theta_mU.mat
+load const_v.mat
 
 R = [0.01, 0.02 0.04 0.08 0.25 0.4 0.01, 0.02 0.04 0.08 0.25 0.4 0.01, 0.02 0.04 0.08 0.25 0.4 0.01, 0.02 0.04 0.08 0.25 0.4];
 
@@ -10,4 +12,16 @@ for indexO = 1: length(t_c)
     for indexI = 1: length(t_c{indexO})
         L(indexO, indexI) = contourLplot(iosT{indexO, indexI}, 1, R(indexO));
     end
+end
+
+% T = cell2mat(t_c);
+% plot(T', L')
+
+hold on
+for indexd = 1:length(t_c)
+    time = t_c{indexd};
+    eL = L(indexd, :);
+    uin_d_1 = (R(indexd)+eL).*(eL.^3)./(mU(indexd).*theta(indexd).*(g(indexd)*0.0016*7).*(R(indexd)+theta(indexd).*0.1));
+%     uin_d_1 = (R(indexd)+eL).*(eL.^3)./(mU(indexd).*theta(indexd).*(g(indexd)*0.0016*7).*(R(indexd)+theta(indexd).*0.15));
+    plot(time, power(uin_d_1, 1/3), '.')
 end
